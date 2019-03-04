85d987aea60b79de2fcec911bacd0ddc193b37d4
Robert Bossy <Robert.Bossy@inra.fr>
2019-03-04 09:15:43 +0100

    privatized methods and removed dead code from BioNLPSTParser

--- bionlp-st-core/src/main/java/org/bionlpst/corpus/parser/bionlpst/BioNLPSTParser.java
+++ bionlp-st-core/src/main/java/org/bionlpst/corpus/parser/bionlpst/BioNLPSTParser.java
@@ -2,7 +2,6 @@ package org.bionlpst.corpus.parser.bionlpst;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -29,41 +28,14 @@ public class BioNLPSTParser {
 	
 	private static final String[] EXTS_ALL = { EXT_CONTENTS, EXT_INPUT, EXT_OUTPUT };
 	private static final String[] EXTS_OUTPUT_ONLY = { EXT_CONTENTS, EXT_INPUT, EXT_OUTPUT };
-	
-	/**
-	 * Computes a document identifier from the specified file according to the BioNLP-ST file naming conventions.
-	 * @param path file.
-	 * @return the document identifier corresponding to the specified file.
-	 */
-	public static String getDocumentIdFromPath(File path) {
-		return getDocumentIdFromPath(path.getName());
-	}
 
-	/**
-	 * Computes a document identifier from the specified path according to the BioNLP-ST file naming conventions.
-	 * @param path path.
-	 * @return the document identifier corresponding to the specified path.
-	 */
-	public static String getDocumentIdFromPath(String path) {
+	private static String getDocumentIdFromPath(String path) {
 		int dot = path.lastIndexOf('.');
 		if (dot == -1) {
 			return path;
 		}
 		int slash = path.lastIndexOf(File.separatorChar);
 		return path.substring(slash + 1, dot);
-	}
-
-	/**
-	 * Parses a file in the BioNLP-ST annotation format. The file name must follow BioNLP-ST file naming conventions. This corpus must already contain a document with the identifier corresponding to the specified file name.
-	 * @param logger message container where to store parse warnings and errors.
-	 * @param asetSelect specification of the annotation set.
-	 * @param file file to parse.
-	 * @throws IOException if there's a I/O error reading the specified file.
-	 */
-	public static void parseAnnotations(CheckLogger logger, Corpus corpus, AnnotationSetSelector asetSelect, File file) throws IOException {
-		try (Reader r = new FileReader(file)) {
-			parseAnnotations(logger, corpus, asetSelect, file.getPath(), r);
-		}
 	}
 
 	/**
@@ -74,7 +46,7 @@ public class BioNLPSTParser {
 	 * @param r stream to parse.
 	 * @throws IOException if there's a I/O error reading the specified file.
 	 */
-	public static void parseAnnotations(CheckLogger logger, Corpus corpus, AnnotationSetSelector asetSelect, String source, Reader r) throws IOException {
+	private static void parseAnnotations(CheckLogger logger, Corpus corpus, AnnotationSetSelector asetSelect, String source, Reader r) throws IOException {
 		String docId = getDocumentIdFromPath(source);
 		if (!corpus.hasDocument(docId)) {
 			logger.serious(new Location(source, -1), "unknown document " + docId);
@@ -86,14 +58,6 @@ public class BioNLPSTParser {
 		aset.parseAnnotations(logger, source, reader);
 	}
 
-	public static Document newDocument(Corpus corpus, File file) throws IOException, BioNLPSTException {
-		try (Reader reader = new FileReader(file)) {
-			String id  = getDocumentIdFromPath(file);
-			String contents = Util.readWholeStream(reader);
-			return new Document(corpus, id, contents);
-		}
-	}
-	
 	public static void getCorpusAndReference(CheckLogger logger, CorpusSource corpusSource, Corpus corpus, boolean loadOutput) throws BioNLPSTException, IOException {
 		Collection<EntryRecord> records = collectEntries(corpusSource, EXTS_ALL);
 		loadDocuments(corpus, records);
@@ -102,7 +66,7 @@ public class BioNLPSTParser {
 			loadOutputAnnotations(logger, corpus, records, AnnotationSetSelector.REFERENCE);
 		}
 	}
-	
+
 	public static Corpus getCorpusAndReference(CheckLogger logger, CorpusSource corpusSource, boolean loadOutput) throws BioNLPSTException, IOException {
 		Corpus result = new Corpus();
 		getCorpusAndReference(logger, corpusSource, result, loadOutput);
